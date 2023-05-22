10c1c27f1648494b7b58872dcac0cb045695d3ee
Pacool84 <flopez_1384@hotmail.com>
2023-05-21 22:04:48 -0600

    [ADD] Primer programa en python

---
+++ 01_print.py
@@ -0,0 +1,13 @@
+#Este es mi primer programa en Python
+
+print("Hello my friend Python")
+
+print(12 + 5)
+print(10-6)
+print(2*8)
+print(20/2)
+
+'''
+Asi se puede realizar un comentario mas extenso
+no importando los saltos de lineas
+'''

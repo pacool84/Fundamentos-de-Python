a90a5d1d8ef0f051c806815744436340ba73e086
Pacool84 <flopez_1384@hotmail.com>
2023-05-21 22:16:57 -0600

    [ADD] Tipo y uso de variables, uso de funcion Input

---
+++ 02:vars.py
@@ -0,0 +1,18 @@
+print("Hello my friends Vars")
+my_name = "Paco"
+print("Hello my name is", my_name)
+
+my_age = 39
+print("My Age is", my_age)
+
+my_name = "Sebastian"
+print("Aqui cambiamos la variable por " + my_name)
+
+# Para solicitar ingresar valores de forma dinamica utilizamos INPUT
+
+my_name = input("Ingresa tu nombre completo:")
+my_age = input("Ingresa tu edad:")
+
+print("Nueva informacion registrada")
+print("Mi nombres es", my_name)
+print("Mi edad es ", my_age, "años")

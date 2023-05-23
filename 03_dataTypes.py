e902112a5fb1afa972046adfa596784b54c3bdc7
Pacool84 <flopez_1384@hotmail.com>
2023-05-22 20:44:33 -0600

    [ADD] Tipo de datos y funcion type

---
+++ 03_dataTypes.py
@@ -0,0 +1,25 @@
+# String
+my_name = "Paco"
+my_other_name = "Sebastian"
+print(my_name)
+print(my_other_name)
+
+# Saber que tipo de dato es la variable
+print(type(my_name))
+
+# integers / enteros
+my_age = 39
+print(my_age)
+print(type(my_age))
+
+# booleans / buleanos
+
+is_single = True
+print(is_single)
+print(type(is_single))
+
+# inputs
+
+my_age = input("Cual es tu edad?")
+print("La edad registrada es", my_age)
+print(type(my_age))

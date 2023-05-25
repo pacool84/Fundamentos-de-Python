64a4ad40bc0e230d62a23142731b886917590af7
Pacool84 <flopez_1384@hotmail.com>
2023-05-24 23:00:37 -0600

    [ADD] Trabajando con booleanos

---
+++ 06_booleans.py
@@ -0,0 +1,11 @@
+is_single = False
+print(type(is_single))
+print(is_single)
+
+# Con el operador not podemos cambiar el valor de una variable
+
+print(not (is_single))
+
+is_single = not (is_single)
+
+print(is_single)

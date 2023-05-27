fd8b813482d353d2e5b824af95a6953862969b2c
Pacool84 <flopez_1384@hotmail.com>
2023-05-26 22:53:34 -0600

    [ADD] Operadores de comparacion

---
+++ 09_comparison_operators.py
@@ -0,0 +1,33 @@
+# Operador de Mayor o Menor que
+
+print(7 > 3)  # Este operador regresa un booleano
+print(3 > 7)
+
+print(5 < 6)
+print(6 < 5)
+
+
+print(10 >= 10)
+print(63 <= 63)
+
+
+# Operador de Igualdad
+
+print(6 == 6)
+print(5 == 2)
+
+# Operador de Diferencial
+
+print(6 != 10)
+print(6 != 6)
+
+# Trabajando los operadores con otro tipo de datos
+
+print("Paco" == "Paco")
+print("paco" == "Paco")  # Considerar que es case sensitive
+
+print("1" == 1)
+
+
+age = 17
+print(age >= 18)

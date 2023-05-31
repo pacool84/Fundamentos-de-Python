61bcdee5a8a05cd9d135a34e70d97837c05f6111
Pacool84 <flopez_1384@hotmail.com>
2023-05-30 21:37:28 -0600

    [ADD] Juego de papel, piedra o tijeras

---
+++ rock_paper_siccors.py
@@ -0,0 +1,28 @@
+user_option = input("piedra, papel o tijera => ")
+computer_option = 'papel'
+
+if user_option == computer_option:
+    print('Empate')
+elif user_option == "piedra":
+    if computer_option == "tijera":
+        print('Piedra gana a tijera')
+        print('Usuario gano!')
+    else:
+        print('papel gana a piedra')
+        print('Computador gano')
+
+elif user_option == "papel":
+    if computer_option == "piedra":
+      print('papel gana a piedra')
+      print('usuario gano')
+    else:
+        print('tijera gana papel')
+        print('Computador gano')
+
+elif user_option =='tijera':
+    if computer_option == 'papel':
+      print('tijera gana a papel')
+      print('Usuario gano')
+    else:
+        print('piedra gana a tijera')
+        print('Computador gano')
